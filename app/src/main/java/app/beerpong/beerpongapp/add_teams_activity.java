3acaa4e6b01e507268cd602cc0a9206297229f1a
jacob <jacos16@student.sdu.dk>
2018-04-03 18:37:08 +0200

    Butterknife

--- app/src/main/java/app/beerpong/beerpongapp/add_teams_activity.java
+++ app/src/main/java/app/beerpong/beerpongapp/add_teams_activity.java
@@ -2,17 +2,25 @@ package app.beerpong.beerpongapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class add_teams_activity extends AppCompatActivity {
+
+    @BindView(R.id.testButton)
+    public Button testButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_teams_activity);
-        //sdklfkldsf
+        ButterKnife.bind(this);
     }
 
     private void stuffDo() {
+        System.out.println("test");
 
     }
 
--- app/src/main/java/app/beerpong/beerpongapp/current_match_activity.java
+++ app/src/main/java/app/beerpong/beerpongapp/current_match_activity.java
@@ -9,5 +9,7 @@ public class current_match_activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_current_match_activity);
+
+        
     }
 }
