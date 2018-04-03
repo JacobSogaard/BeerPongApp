f9eacfe2b2bedc894d3b5843e11f2b20dcb828b7
jacob <jacos16@student.sdu.dk>
2018-04-03 17:57:05 +0200

    Init

---
+++ app/src/main/java/app/beerpong/beerpongapp/add_teams_activity.java
@@ -0,0 +1,16 @@
+package app.beerpong.beerpongapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class add_teams_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_teams_activity);
+        //sdklfkldsf
+    }
+
+
+}
