36ccc9bbfda9d12265f46a745a5f615e4e5c05f0
jacob <jacos16@student.sdu.dk>
2018-04-16 18:41:18 +0200

    Butterknife

--- app/src/main/java/app/beerpong/beerpongapp/current_match_activity.java
+++ app/src/main/java/app/beerpong/beerpongapp/current_match_activity.java
@@ -2,14 +2,26 @@ package app.beerpong.beerpongapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class current_match_activity extends AppCompatActivity {
+
+    @BindView(R.id.matchScreenTitle)
+    public TextView match_screen_title;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_current_match_activity);
+        ButterKnife.bind(this);
 
+        String matchTitle = String.format(getResources().getString(R.string.match_screen_title), "TITLE");
+        this.match_screen_title.setText(matchTitle);
         
     }
 }
